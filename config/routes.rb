52afd33295168ecc42e7c8b20cbc547ed1b559b2
shin <5446shin@gmail.com>
2019-04-08 20:07:58 +0900

    add: diviseを使いログインシステムを一括導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'videos#index'
   resources :videos
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
