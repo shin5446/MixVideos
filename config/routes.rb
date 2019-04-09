65d83ff1aebd8fbe8628ac58be1e30b6d6425a41
shin <5446shin@gmail.com>
2019-04-09 13:13:26 +0900

    add: ユーザーの詳細画面を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  resources :users, only: [:show]
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root 'videos#index'
   resources :videos
   if Rails.env.development?
