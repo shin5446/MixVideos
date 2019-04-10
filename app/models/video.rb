2d42718609e8fda735264434119c13ca87571da9
shin <5446shin@gmail.com>
2019-04-10 19:26:34 +0900

    fix:いいね機能のアソシエーションを修正

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  has_many :favorite_users, through: :favorites, source: :user
+  has_many :like_users, through: :likes, source: :user
   has_many :genre_subscs, dependent: :destroy
   has_many :genres, through: :genre_videos, source: :genre
 end
