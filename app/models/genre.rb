ed5ace75971e64a9a5e85f70668dd0089fffda12
shin <5446shin@gmail.com>
2019-04-10 18:31:17 +0900

    add: アソシエーションの定義

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+  has_many :genre_videos, dependent: :destroy
+  has_many :videos, through: :genre_videos, source: :video
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,6 @@ class Video < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  has_many :genre_subscs, dependent: :destroy
+  has_many :genres, through: :genre_videos, source: :genre
 end
