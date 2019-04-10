30157ece2047f5a8f64db77f74dfde97012a6137
shin <5446shin@gmail.com>
2019-04-10 15:05:03 +0900

    add: 管理者を識別するためのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_065524) do
+ActiveRecord::Schema.define(version: 2019_04_10_060207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_04_09_065524) do
     t.string "name"
     t.string "icon"
     t.text "introduction"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
