d346d5eacb0eb75ced6348b25cc4645b31c399eb
shin <5446shin@gmail.com>
2019-04-10 19:28:51 +0900

    add: ジャンルのシードデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Genre.create([
+  {name: 'お笑い'},
+  {name: 'スポーツ'},
+  {name: '学習'},
+  {name: '料理'},
+  {name: '生き物'},
+  {name: '音楽'},
+  {name: '趣味'},
+  {name: 'メイク、ファッション'},
+  {name: 'ゲーム'},
+  {name: 'アニメ'},
+  {name: '政治、ニュース'},
+  {name: '海外'},
+  {name: 'サブカルチャー'},
+  {name: 'ユーチューバー'},
+  {name: '雑学'},
+  {name: 'フィットネス'},
+  {name: '旅'}
+])
