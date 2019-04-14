865057b3dbdc9d7777332b39b8d87120914b2bc7
shin <5446shin@gmail.com>
2019-04-14 21:14:28 +0900

    add: モデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_videos, through: :likes, source: :video
   has_many :comments
+
+  validates :name, presence: true, length: { maximum: 30 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/video.rb
+++ app/models/video.rb
@@ -5,4 +5,9 @@ class Video < ApplicationRecord
   has_many :video_genres, dependent: :destroy
   has_many :genres, through: :video_genres, source: :genre
   has_many :comments, dependent: :destroy
+
+  validates :title, :content, :url, presence: true
+  validates :title, length: { maximum: 30 }
+  validates :content, length: { maximum: 300 }
+  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/, length: { maximum: 100 }
 end
