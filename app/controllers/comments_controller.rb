0a8b0c618c6e5669b35db2328cdb079b74c57cb2
shin <5446shin@gmail.com>
2019-04-14 19:04:24 +0900

    add: add: コメントの投稿者がわかるように実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,26 @@ class CommentsController < ApplicationController
   def create
     @video = Video.find(params[:video_id])
     @comment = @video.comments.build(comment_params)
+    @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
         format.js { render :index }
       else
-        raise
         format.html { redirect_to video_path(@video), notice: '投稿できませんでした...' }
       end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render :index
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:video_id, :content)
+    params.require(:comment).permit(:user_id, :video_id, :content)
   end
 end
