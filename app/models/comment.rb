984d03fb8da84526b215858be336675bc15706af
shin <5446shin@gmail.com>
2019-04-14 18:17:40 +0900

    add: CommentとUserモデル間のアソシエーションを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :video
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :videos
   has_many :likes, dependent: :destroy
   has_many :like_videos, through: :likes, source: :video
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
