57adfeb6f824c741479714d09a62c03d0fd4e65a
shin <5446shin@gmail.com>
2019-04-14 18:09:57 +0900

    add: commentsテーブルにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_064238) do
+ActiveRecord::Schema.define(version: 2019_04_14_085924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_04_14_064238) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
     t.index ["video_id"], name: "index_comments_on_video_id"
   end
 
@@ -77,6 +79,7 @@ ActiveRecord::Schema.define(version: 2019_04_14_064238) do
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "comments", "videos"
   add_foreign_key "video_genres", "genres"
   add_foreign_key "video_genres", "videos"
