ca5e7b9e442c783660c1e4f6a901c3d9c0833b81
shin <5446shin@gmail.com>
2019-04-15 12:40:55 +0900

    add: relationshipsのdestroyメソッドを作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,5 +9,8 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    @user
   end
 end
