ab3c8e022c6b75a76097b2f656390993b6990a18
shin <5446shin@gmail.com>
2019-04-15 12:23:22 +0900

    add: createメソッドの処理を実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,11 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  respond_to? :js
+
   def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    @user
   end
 
   def destroy
