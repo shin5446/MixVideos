e1e3c4521328790bb9e6d95de7c7f35a588a167d
shin <5446shin@gmail.com>
2019-04-15 12:42:45 +0900

    add: unfollowメソッドをモデルに定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
   def following?(other_user)
     active_relationships.find_by(followed_id: other_user.id)
   end
+  #指定のユーザのフォローを解除する
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
