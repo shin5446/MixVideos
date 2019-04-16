cc30dd49b0016bd295150c88a622ed8d72394a17
shin <5446shin@gmail.com>
2019-04-16 19:36:50 +0900

    add: いいね数を表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_055840) do
+ActiveRecord::Schema.define(version: 2019_04_16_100317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_055840) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "likes_count"
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
