33aac2f0a0e710d16d7ac318504f31f2a404700f
shin <5446shin@gmail.com>
2019-04-19 10:42:08 +0900

    add:　通常新規作成時にUIDカラムが自動生成されるように設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  # 新規登録時にUIDカラムを自動生成
+  def build_resource(hash = {})
+    hash[:uid] = User.create_unique_string
+    super
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
