035327c8e064a59600615b1b1aa31b9377e5e16f
shin <5446shin@gmail.com>
2019-04-22 14:20:22 +0900

    add: タグ一覧ページにページネーションを実装

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
+  PER_PAGE = 200
+
   def index
-    @tags = Video.all_tags.order(taggings_count: :desc)
+    @tags = Video.all_tags.page(params[:page]).per(PER_PAGE).order(taggings_count: :desc)
   end
 end
