4c9c6445de43e68d0b3a468b17d014d9a16a913b
shin <5446shin@gmail.com>
2019-04-22 20:04:21 +0900

    add: featurespecの実装を追加

--- spec/factories/video.rb
+++ spec/factories/video.rb
@@ -2,21 +2,25 @@ FactoryBot.define do
   factory :video do
     title { 'test_video_01' }
     content { 'testtesttest' }
+    url { 'https://www.youtube.com/watch?v=T79RaXB2zdo' }
   end
 
   factory :second_video, class: Video do
     title { 'test_video_02' }
     content { 'samplesample' }
+    url { 'https://www.youtube.com/watch?v=18cdgGnUhqU' }
   end
 
   factory :third_video, class: Video do
     title { 'test_video_03' }
     content { 'samplesample' }
+    url { 'https://www.youtube.com/watch?v=rmvpnCY_FYQ' }
   end
 
   factory :fourth_video, class: Video do
     title { 'test_video_04' }
     content { 'samplesample' }
+    url { 'https://www.youtube.com/watch?v=S9qp7QMCabM' }
   end
 
   factory :user_video, class: Video do
--- spec/features/video.spec.rb
+++ spec/features/video.spec.rb
@@ -4,8 +4,12 @@ require 'date'
 RSpec.feature '動画機能', type: :feature do
   describe '一覧表示機能' do
     background do
-      # 一般ユーザー1の作成
-      FactoryBot.create(:user)
+      user_a = FactoryBot.create(:user)
+      FactoryBot.create(:video, user: user_a)
+      FactoryBot.create(:second_video, user: user_a)
+      FactoryBot.create(:third_video, user: user_a)
+      FactoryBot.create(:fourth_video, user: user_a)
+
     end
     context 'ユーザー１がログインしている時' do
       before do
@@ -13,6 +17,7 @@ RSpec.feature '動画機能', type: :feature do
         fill_in 'メールアドレス', with: 'user1@example.com'
         fill_in 'パスワード', with: 'password'
         click_button 'Log in'
+        visit videos_path
       end
 
       scenario 'ユーザー１でログインしているときに名前が表示されるかテスト' do
@@ -22,11 +27,19 @@ RSpec.feature '動画機能', type: :feature do
 
       scenario '動画作成のテスト' do
         visit new_video_path
-        fill_in 'video_title', with: 'あああ'
-        fill_in 'video[content]', with: 'いいい'
+        fill_in 'video_title', with: 'テストタイトル'
+        fill_in 'video[content]', with: 'テストコンテント'
+        fill_in 'video[url]', with: 'https://www.youtube.com/watch?v=ffyYxu1rPi8'
         click_on '登録する'
-        expect(page).to have_content 'あああ'
-        expect(page).to have_content 'いいい'
+        expect(page).to have_content 'テストタイトル'
+        expect(page).to have_content 'テストコンテント'
+      end
+    end
+    context '動画のソート、検索' do
+      scenario '新着順の動画ソートのテスト' do
+        visit videos_path
+        click_link '新着順'
+        expect(Video.order('created_at ASC').map(&:title)).to eq %w[test_video_01 test_video_02 test_video_03 test_video_04]
       end
     end
   end
