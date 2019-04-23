0c7afa81cf7fd2c8a3d960452220707fd9786bd7
shin <5446shin@gmail.com>
2019-04-23 11:00:57 +0900

    fix: マイグレーションファイルの修正

--- db/migrate/20190418091936_add_column_to_user_for_oauth.rb
+++ db/migrate/20190418091936_add_column_to_user_for_oauth.rb
@@ -2,7 +2,6 @@ class AddColumnToUserForOauth < ActiveRecord::Migration[5.2]
   def change
       add_column :users, :provider, :string, null: false, default: ""
       add_column :users, :uid, :string, null: false, default: ""
-    end
-    add_index :users, [:uid, :provider], unique: true
+      add_index :users, [:uid, :provider], unique: true
   end
 end
