c1cb6f3644a95fde903607a0c06907df78ab39d4
shin <5446shin@gmail.com>
2019-04-23 10:14:46 +0900

    add:削除してしまったマイグレーションファイルを復活

---
+++ db/migrate/20190418091936_Add_Column_To_UserFor_Oauth.rb
@@ -0,0 +1,9 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :provider, null: false, default: ""
+      t.string :uid, null: false, default: ""
+    end
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
