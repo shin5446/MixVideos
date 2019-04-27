aca8bd523631a3c5366ed4a0beab5c69a3239771
shin <5446shin@gmail.com>
2019-04-28 06:59:55 +0900

    add: ユーザー情報のパーシャルを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    type = params[:type]
+
+    if type == 'post'
+      @post_videos = @user.videos
+    elsif type == 'like'
+      @like_videos = @user.like_videos
+    elsif type == 'following'
+      @following = @user.following
+    elsif type == 'followers'
+      @followers = @user.followers
+    end
   end
 end
