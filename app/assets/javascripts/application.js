e32f19c6bd1002764091429dbad6e68cffb1ba0c
shin <5446shin@gmail.com>
2019-04-28 03:04:05 +0900

    change: ユーザーの編集画面を変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,27 @@
 //= require jquery-ui
 //= require tag-it
 //= require_tree .
+
+// ユーザーアイコンの編集デザイン
+$(function(){
+  $fileField = $('#file')
+
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "100%",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
