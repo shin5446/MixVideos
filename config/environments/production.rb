87497d405fbe03f71767a9c4cdb8567dca719156
shin <5446shin@gmail.com>
2019-05-18 13:22:15 +0900

    fix: 本番環境のホストを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'serene-eyrie-65716.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://mixvideos.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
